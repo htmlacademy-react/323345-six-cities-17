09735cbf5f9011f461337e0f5aea45ccb7a5ec21
Sergey Ivanov <licos.87@mail.ru>
2024-12-07 21:23:31 +0300

    refactor: Исправляет ошибку линтера в App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,8 +19,10 @@ function App({offersList}: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={RoutePath.MAIN}
-               element={<MainLayout isAuthenticated={AUTHENTICATED} favoritesList={FAVORITES_LIST_MOCK}/>}>
+        <Route
+          path={RoutePath.MAIN}
+          element={<MainLayout isAuthenticated={AUTHENTICATED} favoritesList={FAVORITES_LIST_MOCK}/>}
+        >
           <Route
             index
             element={
