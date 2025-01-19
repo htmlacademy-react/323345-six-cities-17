8ab195b74f5fa1af7f9d329b7b4de28782052fee
Sergey Ivanov <licos.87@mail.ru>
2025-01-19 16:05:24 +0300

    fix: Правит ошибки линтера

--- src/shared/utils/response-adapter/response-to-offer-type-adapter.ts
+++ src/shared/utils/response-adapter/response-to-offer-type-adapter.ts
@@ -3,24 +3,22 @@ import { ResponseOfferType } from '../../types/types/response-offer-type';
 
 export const responseToOfferTypeAdapter = (
   responseData: ResponseOfferType
-): OfferType => {
-  return {
-    id: responseData.id,
-    title: responseData.title,
-    type: <'house' | 'hotel' | 'room' | 'apartment'>responseData.type,
-    price: responseData.price,
-    city: {
-      name: responseData.city.name,
-      location: responseData.city.location,
-    },
-    location: {
-      latitude: responseData.location.latitude,
-      longitude: responseData.location.longitude,
-      zoom: responseData.location.zoom,
-    },
-    isFavorite: responseData.isFavorite,
-    isPremium: responseData.isPremium,
-    rating: responseData.rating,
-    previewImage: responseData.previewImage,
-  };
-};
+): OfferType => ({
+  id: responseData.id,
+  title: responseData.title,
+  type: <'house' | 'hotel' | 'room' | 'apartment'>responseData.type,
+  price: responseData.price,
+  city: {
+    name: responseData.city.name,
+    location: responseData.city.location,
+  },
+  location: {
+    latitude: responseData.location.latitude,
+    longitude: responseData.location.longitude,
+    zoom: responseData.location.zoom,
+  },
+  isFavorite: responseData.isFavorite,
+  isPremium: responseData.isPremium,
+  rating: responseData.rating,
+  previewImage: responseData.previewImage,
+});
