57206dd1ef0500b922c1397b76534c6f405eb417
Sergey Ivanov <licos.87@mail.ru>
2025-01-27 18:22:22 +0300

    fix: Правит ошибки линтера

--- src/widgets/header/ui/components/header-nav.tsx
+++ src/widgets/header/ui/components/header-nav.tsx
@@ -10,43 +10,43 @@ import { selectLoadFavoriteOffers } from '../../../../store/reducer/favorite/sel
 import { selectUser } from '../../../../store/reducer/user/selectors/select-user.ts';
 
 function HeaderNavTemplate(): JSX.Element {
-	const dispatch = useAppDispatch();
-	const favoritesList = useAppSelector(selectLoadFavoriteOffers);
-	const user = useAppSelector(selectUser);
-	function logout() {
-		dispatch(logoutAction());
-	}
+  const dispatch = useAppDispatch();
+  const favoritesList = useAppSelector(selectLoadFavoriteOffers);
+  const user = useAppSelector(selectUser);
+  function logout() {
+    dispatch(logoutAction());
+  }
 
-	return (
-		<nav className="header__nav">
-			<ul className="header__nav-list">
-				<li className="header__nav-item user">
-					<NavLink className={styles.userLink} to={RoutePath.FAVORITES}>
-						<span className="header__nav-link header__nav-link--profile">
-							<div className="header__avatar-wrapper user__avatar-wrapper">
-								<img
-									className="reviews__avatar user__avatar"
-									src={user.avatarUrl}
-									width="54"
-									height="54"
-									alt="Reviews avatar"
-								/>
-							</div>
-							<span className="header__user-name user__name">
-								{user.name}
-							</span>
-						</span>
-						<span className="header__favorite-count">{favoritesList?.length}</span>
-					</NavLink>
-				</li>
-				<li className="header__nav-item" onClick={logout}>
-					<div className="header__nav-link">
-						<span className="header__signout">Sign out</span>
-					</div>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="header__nav">
+      <ul className="header__nav-list">
+        <li className="header__nav-item user">
+          <NavLink className={styles.userLink} to={RoutePath.FAVORITES}>
+            <span className="header__nav-link header__nav-link--profile">
+              <div className="header__avatar-wrapper user__avatar-wrapper">
+                <img
+                  className="reviews__avatar user__avatar"
+                  src={user.avatarUrl}
+                  width="54"
+                  height="54"
+                  alt="Reviews avatar"
+                />
+              </div>
+              <span className="header__user-name user__name">
+                {user.name}
+              </span>
+            </span>
+            <span className="header__favorite-count">{favoritesList?.length}</span>
+          </NavLink>
+        </li>
+        <li className="header__nav-item" onClick={logout}>
+          <div className="header__nav-link">
+            <span className="header__signout">Sign out</span>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 const HeaderNav = memo(HeaderNavTemplate);
